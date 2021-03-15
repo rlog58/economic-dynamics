8d84616d922a055d015e8664d17bfa6e8b91eab4
Roman Logoida <61096583+rlog58@users.noreply.github.com>
2021-03-15 20:24:21 +0300

    model structure changed

--- src/main/java/model/ramseycasskoopmans/RamseyCassKoopmansModel.java
+++ src/main/java/model/ramseycasskoopmans/RamseyCassKoopmansModel.java
@@ -6,14 +6,25 @@ import util.Model;
 public class RamseyCassKoopmansModel extends Model {
 
     public RamseyCassKoopmansModel(Double n, Double g, Double ro, Double theta, Double alpha) {
-        this.eqSystem = new Expression[]{
-                // dk/dt
-                (args) -> (Math.pow(args[0], alpha) - args[1] - (n + g) * args[0]),
-                // dc/dt
-                (args) -> (args[1] * (alpha * Math.pow(args[0], alpha - 1) - ro - theta * g) / theta)
-        };
 
-        this.resSystem = new Expression[]{};
+        this.eqSystem = new Expression[]{};
+
+        this.dSystem = new Expression[]{
+                // k
+                (args) -> {
+                    Double k = args[0];
+                    Double c = args[1];
+
+                    return Math.pow(k, alpha) - c - (n + g) * k;
+                },
+                // c
+                (args) -> {
+                    Double k = args[0];
+                    Double c = args[1];
+
+                    return c * (alpha * Math.pow(k, alpha - 1) - ro - theta * g) / theta;
+                }
+        };
 
     }
 
--- src/main/java/model/ramseycasskoopmans/RamseyCassKoopmansPlot.java
+++ src/main/java/model/ramseycasskoopmans/RamseyCassKoopmansPlot.java
@@ -8,89 +8,76 @@ import java.util.Arrays;
 
 public class RamseyCassKoopmansPlot {
 
-    public static void plotPhasePortrait() {
-        Plot plot = new Plot("Phase portrait",
-                "k",
-                "c",
-                true,
-                true);
+    public static void plot1() {
+        Plot plot = new Plot("Phase portrait", "k", "c", true, true);
 
         RamseyCassKoopmansModel model = new RamseyCassKoopmansModel(0.01, 0.02, 0.04, 0.3, 0.5);
 
         DSolver dSolver = new DSolver(0.0, 100.0, 0.01);
 
-        Double[][] data = dSolver.solve(model.getEqSystem(), new Double[]{100.0, 5.8});
+        Double[][] data = dSolver.solve(model.getDSystem(), new Double[]{100.0, 5.8});
         plot.addPlotXY(data, "(k0 = 100; c0 = 5.8)");
 
-        data = dSolver.solve(model.getEqSystem(), new Double[]{100.0, 5.85});
+        data = dSolver.solve(model.getDSystem(), new Double[]{100.0, 5.85});
         plot.addPlotXY(data, "(k0 = 100; c0 = 5.85)");
 
-        data = dSolver.solve(model.getEqSystem(), new Double[]{100.0, 5.90});
+        data = dSolver.solve(model.getDSystem(), new Double[]{100.0, 5.90});
         plot.addPlotXY(data, "(k0 = 100; c0 = 5.9)");
 
-        data = dSolver.solve(model.getEqSystem(), new Double[]{100.0, 5.95});
+        data = dSolver.solve(model.getDSystem(), new Double[]{100.0, 5.95});
         plot.addPlotXY(data, "(k0 = 100; c0 = 5.95)");
 
-        data = dSolver.solve(model.getEqSystem(), new Double[]{100.0, 6.00});
+        data = dSolver.solve(model.getDSystem(), new Double[]{100.0, 6.00});
         plot.addPlotXY(data, "(k0 = 100; c0 = 6.0)");
 
         plot.show();
     }
 
-    public static void plotReducedCapital() {
-        Plot plot = new Plot("Reduced capital",
-                "t",
-                "k",
-                true,
-                true);
+    public static void plot2() {
+        Plot plot = new Plot("Reduced capital", "t", "k", true, true);
 
         RamseyCassKoopmansModel model = new RamseyCassKoopmansModel(0.01, 0.02, 0.04, 0.3, 0.5);
 
         DSolver dSolver = new DSolver(0.0, 100.0, 0.01);
 
-        Double[][] data = dSolver.solve(model.getEqSystem(), new Double[]{100.0, 5.8});
+        Double[][] data = dSolver.solve(model.getDSystem(), new Double[]{100.0, 5.8});
         plot.addPlotX(Arrays.stream(data).map(x -> x[0]).toArray(Double[]::new), "(k0 = 100; c0 = 5.8)");
 
-        data = dSolver.solve(model.getEqSystem(), new Double[]{100.0, 5.85});
+        data = dSolver.solve(model.getDSystem(), new Double[]{100.0, 5.85});
         plot.addPlotX(Arrays.stream(data).map(x -> x[0]).toArray(Double[]::new), "(k0 = 100; c0 = 5.85)");
 
-        data = dSolver.solve(model.getEqSystem(), new Double[]{100.0, 5.90});
+        data = dSolver.solve(model.getDSystem(), new Double[]{100.0, 5.90});
         plot.addPlotX(Arrays.stream(data).map(x -> x[0]).toArray(Double[]::new), "(k0 = 100; c0 = 5.9)");
 
-        data = dSolver.solve(model.getEqSystem(), new Double[]{100.0, 5.95});
+        data = dSolver.solve(model.getDSystem(), new Double[]{100.0, 5.95});
         plot.addPlotX(Arrays.stream(data).map(x -> x[0]).toArray(Double[]::new), "(k0 = 100; c0 = 5.95)");
 
-        data = dSolver.solve(model.getEqSystem(), new Double[]{100.0, 6.00});
+        data = dSolver.solve(model.getDSystem(), new Double[]{100.0, 6.00});
         plot.addPlotX(Arrays.stream(data).map(x -> x[0]).toArray(Double[]::new), "(k0 = 100; c0 = 6.0)");
 
         plot.show();
     }
 
-    public static void plotReducedConsumption() {
-        Plot plot = new Plot("Reduced consumption",
-                "t",
-                "c",
-                true,
-                true);
-
+    public static void plot3() {
+        Plot plot = new Plot("Reduced consumption", "t", "c", true, true);
 
         RamseyCassKoopmansModel model = new RamseyCassKoopmansModel(0.01, 0.02, 0.04, 0.3, 0.5);
 
         DSolver dSolver = new DSolver(0.0, 100.0, 0.01);
 
-        Double[][] data = dSolver.solve(model.getEqSystem(), new Double[]{100.0, 5.8});
+        Double[][] data = dSolver.solve(model.getDSystem(), new Double[]{100.0, 5.8});
         plot.addPlotX(Arrays.stream(data).map(x -> x[1]).toArray(Double[]::new), "(k0 = 100; c0 = 5.8)");
 
-        data = dSolver.solve(model.getEqSystem(), new Double[]{100.0, 5.85});
+        data = dSolver.solve(model.getDSystem(), new Double[]{100.0, 5.85});
         plot.addPlotX(Arrays.stream(data).map(x -> x[1]).toArray(Double[]::new), "(k0 = 100; c0 = 5.85)");
 
-        data = dSolver.solve(model.getEqSystem(), new Double[]{100.0, 5.90});
+        data = dSolver.solve(model.getDSystem(), new Double[]{100.0, 5.90});
         plot.addPlotX(Arrays.stream(data).map(x -> x[1]).toArray(Double[]::new), "(k0 = 100; c0 = 5.9)");
 
-        data = dSolver.solve(model.getEqSystem(), new Double[]{100.0, 5.95});
+        data = dSolver.solve(model.getDSystem(), new Double[]{100.0, 5.95});
         plot.addPlotX(Arrays.stream(data).map(x -> x[1]).toArray(Double[]::new), "(k0 = 100; c0 = 5.95)");
 
-        data = dSolver.solve(model.getEqSystem(), new Double[]{100.0, 6.00});
+        data = dSolver.solve(model.getDSystem(), new Double[]{100.0, 6.00});
         plot.addPlotX(Arrays.stream(data).map(x -> x[1]).toArray(Double[]::new), "(k0 = 100; c0 = 6.0)");
 
         plot.show();
@@ -98,11 +85,11 @@ public class RamseyCassKoopmansPlot {
 
     public static void main(String[] args) {
 
-        plotPhasePortrait();
+        plot1();
 
-        plotReducedCapital();
+        plot2();
 
-        plotReducedConsumption();
+        plot3();
 
 
     }
